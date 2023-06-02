39cc94c5c59f6f4465c229a8328923a3becd4a4f
Ike <isaac.chasse@valkyrie.ai>
2023-06-02 16:51:22 -0500

    added a super basic clap functionality to main function - need to create file parsing next for clap functionality

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,14 @@
-use vergen::{CompatibilityOperator, MetaRequirement, PackageMetadata, PackageName, PackageRequirement, VersionSpecification};
+use clap::{command, Arg, ArgAction};
 
 fn main() {
-    // Sample data
-    let package_name = PackageName { name: "example-package".to_owned() };
-    let compatibility_operator = CompatibilityOperator::Exact;
-    let version_specification = VersionSpecification::new(1, 2, 3);
-    let metadata = Some(PackageMetadata {
-        meta_requirement: MetaRequirement::LanguageVersion,
-        compatibility: CompatibilityOperator::GreaterEq,
-        version: VersionSpecification::new(3, 2, 1),
-    });
+    let matches = command!() // requires `cargo` feature
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
 
-    let package_requirement = PackageRequirement {
-        name: package_name,
-        compatibility: compatibility_operator,
-        version: version_specification,
-        metadata: metadata,
-    };
-
-    // Printing structs
-    // println!("Package Name: {}", package_name);
-    // println!("Compatibility Operator: {}", compatibility_operator);
-    // println!("Version Specification: {}", version_specification);
-    // println!("Package Metadata: {}", metadata.unwrap_or_default());
-    println!("Package Requirement: {}", package_requirement);
+    println!("verbose: {:?}", matches.get_flag("verbose"));
 }
